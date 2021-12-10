908605581eb06e2d45032ea8dc8da204af46ee0d
Maxim Babichev <maksim.babichev95@gmail.com>
2021-12-10 19:04:13 +0300

    fix benchmark. mysql docker without rosetta2

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-composer req bavix/laravel-wallet:6.0.0
+
+composer req bavix/laravel-wallet:6.0.4
 composer req bavix/laravel-wallet-vacuum:1.0.1
 composer unit
-mv build/junit{,-6.0.0}.xml
+mv build/junit{,-6.0.4}.xml
 
 sleep 5
 
@@ -14,9 +15,9 @@ mv build/junit{,-6.1.0}.xml
 sleep 5
 
 composer remove bavix/laravel-wallet-vacuum
-composer req bavix/laravel-wallet:6.2.0
+composer req bavix/laravel-wallet:6.2.4
 composer unit
-mv build/junit{,-6.2.0}.xml
+mv build/junit{,-6.2.4}.xml
 
 sleep 5
 
@@ -26,18 +27,18 @@ mv build/junit{,-7.0.0}.xml
 
 sleep 5
 
-composer req bavix/laravel-wallet:7.1.0-RC3
+composer req bavix/laravel-wallet:7.1.0
 composer unit
-mv build/junit{,-7.1.0-RC3}.xml
+mv build/junit{,-7.1.0}.xml
 
 sleep 5
 
-composer req bavix/laravel-wallet:7.2.0-beta1
+composer req bavix/laravel-wallet:7.2.0
 composer unit
-mv build/junit{,-7.2.0-beta1}.xml
+mv build/junit{,-7.2.0}.xml
 
 sleep 5
 
-composer req bavix/laravel-wallet:7.3.0-alpha
+composer req bavix/laravel-wallet:7.3.0-beta1
 composer unit
-mv build/junit{,-7.3.0-alpha}.xml
+mv build/junit{,-7.3.0-beta1}.xml

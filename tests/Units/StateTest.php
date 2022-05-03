65e3e2485c82d948fd08596c6defc91d03e7c1e4
Maxim Babichev <maksim.babichev95@gmail.com>
2022-05-03 20:37:30 +0300

    drop support 6.x

--- tests/Units/StateTest.php
+++ tests/Units/StateTest.php
@@ -4,13 +4,7 @@ declare(strict_types=1);
 
 namespace Bavix\WalletBench\Test\Units;
 
-use Bavix\Wallet\Internal\BookkeeperInterface;
-use Bavix\Wallet\Internal\Service\DatabaseService;
 use Bavix\Wallet\Internal\Service\DatabaseServiceInterface;
-use Bavix\Wallet\Internal\StorageInterface;
-use Bavix\Wallet\Services\BookkeeperService;
-use Bavix\Wallet\Services\DbService;
-use Bavix\Wallet\Services\StorageService;
 use Bavix\WalletBench\Test\Infra\Factories\BuyerFactory;
 use Bavix\WalletBench\Test\Infra\Models\Buyer;
 use Bavix\WalletBench\Test\Infra\TestCase;
@@ -27,25 +21,12 @@ final class StateTest extends TestCase
     {
         /** @var Buyer $buyer */
         $buyer = BuyerFactory::new()->create();
-        $callback = static function () use ($buyer) {
+
+        app(DatabaseServiceInterface::class)->transaction(static function () use ($buyer) {
             for ($i = 0; $i < 256; ++$i) {
                 $buyer->wallet->depositFloat(0.01);
             }
-        };
-
-        if (class_exists(DatabaseService::class)) {
-            app(DatabaseServiceInterface::class)->transaction($callback);
-        } elseif (class_exists(DbService::class)) {
-            if (class_exists(BookkeeperService::class)) {
-                app(BookkeeperInterface::class)->missing($buyer->wallet);
-            }
-
-            if (class_exists(StorageService::class)) {
-                app(StorageInterface::class)->flush();
-            }
-
-            app(DbService::class)->transaction($callback);
-        }
+        });
 
         self::assertSame(256, (int) $buyer->balance);
     }

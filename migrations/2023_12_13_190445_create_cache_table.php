4e20d7b961a5f55ae64199d21e474b315beda622
Maxim Babichev <maksim.babichev95@gmail.com>
2024-02-03 11:50:41 +0300

    fix

---
+++ migrations/2023_12_13_190445_create_cache_table.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class() extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (!Schema::hasTable('cache')) {
+            Schema::create('cache', static function (Blueprint $table) {
+                $table->string('key')
+                    ->primary();
+                $table->mediumText('value');
+                $table->integer('expiration');
+            });
+        }
+
+        if (!Schema::hasTable('cache_locks')) {
+            Schema::create('cache_locks', static function (Blueprint $table) {
+                $table->string('key')
+                    ->primary();
+                $table->string('owner');
+                $table->integer('expiration');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        if (Schema::hasTable('cache')) {
+            Schema::dropIfExists('cache');
+        }
+
+        if (Schema::hasTable('cache_locks')) {
+            Schema::dropIfExists('cache_locks');
+        }
+    }
+};
